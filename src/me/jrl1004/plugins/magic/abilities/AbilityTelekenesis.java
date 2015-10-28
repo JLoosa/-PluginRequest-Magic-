be0f1c54875c1996201a78fd80e5a1b491b02065
JRL1004 <Loosa.J.History@gmail.com>
2015-10-28 16:50:45 -0600

    Small tweak to Telekenesis item and push of plugin jar

--- src/me/jrl1004/plugins/magic/abilities/AbilityTelekenesis.java
+++ src/me/jrl1004/plugins/magic/abilities/AbilityTelekenesis.java
@@ -3,19 +3,20 @@ package me.jrl1004.plugins.magic.abilities;
 import me.jrl1004.plugins.magic.managers.ChatManager;
 import me.jrl1004.plugins.magic.managers.TelekenesisManager;
 
+import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 
 public class AbilityTelekenesis extends AbstractAbility {
 
 	public AbilityTelekenesis() {
-		super("Telekenesis", customStack(Material.ARROW, "Telekenesis", "Click on a player or block to target them", "Click again to lob your target through the air"));
+		super("Telekenesis", customStack(Material.BOOK, ChatColor.GOLD + "Telekenesis", "Click on a player or block to target them", "Click again to lob your target through the air"));
 		setPerm("magic.telekenesis");
 	}
 
 	@Override
 	public void castSpell(Player player) {
-		if(!canUse(player)) {
+		if (!canUse(player)) {
 			ChatManager.messageBad(player, "You are not capable of performing this spell.");
 			return;
 		}
